8d3925d976b36b21b48c2411e0ae7584610fe5d9
Anmol Karki <anmolkarki101@gmail.com>
2025-04-10 15:44:32 +0545

    script for installing git across multiple operating systems

---
+++ gitinstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#this script will check the OS version, its type and accordingly procced with a type of software installation.
+echo "Script to install git"
+echo "Starting Installation..."
+if ["$(uname)"=="Linux"];
+then
+	echo "Linux Detected, installing.."
+#	yum install git -y
+elif ["$(uname)"=="Mac"];
+then	
+	echo "Mac Detected, installing.."
+	brew install git
+else
+	echo "not installing"
+fi
