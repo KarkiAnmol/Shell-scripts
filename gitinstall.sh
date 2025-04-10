639c44698437476fb7e54b472caab4215828f74d
Anmol Karki <anmolkarki101@gmail.com>
2025-04-10 15:54:54 +0545

    spacing fixed

--- gitinstall.sh
+++ gitinstall.sh
@@ -2,11 +2,11 @@
 #this script will check the OS version, its type and accordingly procced with a type of software installation.
 echo "Script to install git"
 echo "Starting Installation..."
-if ["$(uname)"=="Linux"];
+if [ "$(uname)" == "Linux" ];
 then
 	echo "Linux Detected, installing.."
 #	yum install git -y
-elif ["$(uname)"=="Mac"];
+elif [" $(uname)" == "Mac" ];
 then	
 	echo "Mac Detected, installing.."
 	brew install git
